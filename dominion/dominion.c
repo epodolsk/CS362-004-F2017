a2b09877be11ba12543f75efaa8ac2bceaab77c9
Eric Podolsky <podolske@oregonstate.edu>
2017-10-07 14:06:05 -0400

    Introduced bug in adventurer card: do not reshuffle discard if deck is empty

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -662,9 +662,6 @@ int adventurerCard(struct gameState *state, int currentPlayer, int handPos) {
   int cardDrawn;
   int drawntreasure = 0;
   while(drawntreasure<2){
-    if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-      shuffle(currentPlayer, state);
-    }
     drawCard(currentPlayer, state);
     cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
     if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
